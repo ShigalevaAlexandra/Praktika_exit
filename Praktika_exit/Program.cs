3da2a17375f33f33d0afc7efcab3f23d8e801756
Александра <shigaleva04@inbox.ru>
2024-02-24 23:47:59 +0300

    Добавьте файлы проекта.

---
+++ Praktika_exit/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_exit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //описание работы программы
+            Console.Write("Данная программа выполняется пока не введете слово EXIT \r\n\r\n");
+
+            //объявление переменной для хранения вводимого значения
+            string input_str;
+
+            //объявление масисива диалога программы с пользователем
+            string[] words_list = {
+                "Привет)", "Как дела?", "ERROR: связь прервана",
+                "поиск сети...", "подключение...", "обновление..." };
+
+            int index_word = 0; //индекс слова из диалога 
+
+            //цикл для перебора массива word_list до тех пор,
+            //пока пользователь не ввдет слово EXIT
+            do
+            {
+                Console.WriteLine(words_list[index_word]);
+                index_word++;
+                input_str = Console.ReadLine();
+
+                //обработка ошибки выхода за границы массива
+                if (index_word >= words_list.Length) index_word = 0;
+                else continue;
+            }
+            while (input_str != "exit");
+
+            Console.Write("\r\n завершение работы... \r\n\r\n");
+        }
+    }
+}
---
+++ Praktika_exit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Praktika_exit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Praktika_exit")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("018bba7e-37f5-4b48-8105-3fdda2c95929")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

69aff2c66c9fa7a99984c6164348941588ac5941
Александра <shigaleva04@inbox.ru>
2024-02-26 20:14:20 +0300

    update console

--- Praktika_exit/Program.cs
+++ Praktika_exit/Program.cs
@@ -11,7 +11,10 @@ namespace Praktika_exit
         static void Main(string[] args)
         {
             //описание работы программы
-            Console.Write("Данная программа выполняется пока не введете слово EXIT \r\n\r\n");
+            Console.SetCursorPosition(0, 1);
+            Console.Write("   * Данная программа выполняется пока не введете слово EXIT * \r\n\r\n");
+            Console.SetCursorPosition(0, 2);
+            Console.WriteLine(new string('_', Console.WindowWidth));
 
             //объявление переменной для хранения вводимого значения
             string input_str;
@@ -21,15 +24,21 @@ namespace Praktika_exit
                 "Привет)", "Как дела?", "ERROR: связь прервана",
                 "поиск сети...", "подключение...", "обновление..." };
 
-            int index_word = 0; //индекс слова из диалога 
+            int index_word = 0,   //индекс слова из диалога 
+                position_y = 4;   //позиция y
 
             //цикл для перебора массива word_list до тех пор,
             //пока пользователь не ввдет слово EXIT
             do
             {
+                Console.SetCursorPosition(3, position_y);
                 Console.WriteLine(words_list[index_word]);
+                position_y += 2;
                 index_word++;
+
+                Console.SetCursorPosition(3, position_y);
                 input_str = Console.ReadLine();
+                position_y++;
 
                 //обработка ошибки выхода за границы массива
                 if (index_word >= words_list.Length) index_word = 0;
@@ -37,7 +46,7 @@ namespace Praktika_exit
             }
             while (input_str != "exit");
 
-            Console.Write("\r\n завершение работы... \r\n\r\n");
+            Console.Write("\r\n   --> завершение работы... \r\n\r\n");
         }
     }
 }
